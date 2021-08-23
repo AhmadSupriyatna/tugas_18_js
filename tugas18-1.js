a5ccb7739a61ea5cfe301876cb8004c0111d0b7d
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-08-23 12:00:45 +0700

    Tugas 18 Javascript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas18-1.js
@@ -0,0 +1,6 @@
+function tugasDelapanBelas(){
+    var tugas = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+    console.log(tugas);
+    console.log("Output : ",tugas.slice(8,24));
+}
+tugasDelapanBelas();
